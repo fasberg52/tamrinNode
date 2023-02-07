5f081b0cefa695be3828c32a7414c9b89efc3951
mohamamdreza Taherian <taherian.1993@gmail.com>
2023-02-07 21:53:45 +0330

    updated https and path

--- server.js
+++ server.js
@@ -19,3 +19,30 @@ const path = require("path");
 let file = path.parse(__filename);
 
 console.log(file);
+
+/* https module */
+const http = require("http");
+const app = http.createServer();
+const PORT = 3008;
+var rofagha = [
+  { id: 0, name: "mohammadreza taherian" },
+  { id: 1, name: "asghar taherian" },
+  { id: 2, name: "jamshid taherian" },
+];
+app.on("request", (req, res) => {
+  const takRafigh = req.url.split("/");
+  if (takRafigh[1] === "rofagha" && req.method === "GET") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application / json");
+
+    if (takRafigh.length == 3) {
+      const takRafighIndex = Number(takRafigh[2]);
+      res.end(JSON.stringify(rofagha[takRafighIndex]));
+    } else {
+      res.end(JSON.stringify(rofagha));
+    }
+  }
+});
+app.listen(PORT, () => {
+  console.log(`app runing on ${PORT}`);
+});
